6b6c36c2f968af6a44a35044362fc15fecf2b63f
MurashovS <imurasov0@gmail.com>
2023-06-29 20:23:40 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20000;
+        int QuantityMilse = 20;
+        int BonusMilse = price / QuantityMilse;
+        System.out.println(BonusMilse);
+    }
+
+    }
