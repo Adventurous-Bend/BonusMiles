89a3e2d35ff5d4a04d1077e24ba58151a9567ee8
MurashovS <imurasov0@gmail.com>
2023-07-23 17:31:27 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int price = 20000;
-        int QuantityMilse = 20;
-        int BonusMilse = price / QuantityMilse;
-        System.out.println(BonusMilse);
+        int quantityMilse = 20;
+        int bonusMilse = price / quantityMilse;
+        System.out.println(bonusMilse);
     }
 
-    }
+}
